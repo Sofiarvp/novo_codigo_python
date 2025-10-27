28817dbcd04d44349aec8d10990160f23ae9ad11
Sofiarvp <sofiarvp77@gmail.com>
2025-10-27 17:29:05 -0300

    Update lero.py

--- anotaçoes_classes/lero.py
+++ anotaçoes_classes/lero.py
@@ -1,14 +1,24 @@
 #PROGRAMAÇAO ORIENTADA A OBJETOS (POO)
 
+#########LINGUAGENS
+#baixo nivel é linguagem decimal (010111010) de maquina
+#linear é a linguagem q n pode ser modificada (ainda na linguagem de máquina)
+#estrutural usa algoritmos e linguagem humana mas n era interativa (entre coisas)
+#programação orientada a objeto é transformar algo físico em computacional (lógico)
+
+
+
 # classes sao estruturas de dados q possuem atributos e metodos (representaçao computacional)----- OBJETOS
-#atributo sao caracteristicas gerais do objeto trabalhado em questao q sao especificados posteriormente ----- VARIAVEIS
+#atributo sao caracteristicas do objeto trabalhado em questao q sao especificados posteriormente ----- VARIAVEIS
 #metodo sao as funçoes q ele vai executar/já executa ----- FUNÇOES
 #classes podem ser iguais mas com variaveis e metodos diferentes
-#classe tem q ta com primeira letra maiuscula (ex.:Carro)
+#classe tem q ta com primeira letra maiuscula (ex.:CarroDrive) pascal case
 #instancias
 
 
 #carro (classe)
+
+# ATRIBUTO -> VARIAVEL
 # modelo -> ferrari
 # ano -> 2020
 # estado -> seminovo
@@ -17,13 +27,13 @@
 # acelerar, frear, buzinar
 # ^^^^^^Metodos^^^^^^
  
-------------------------------------------
-#def define funçao
-#__init__ 'dois underlines'
-#self referencia ao proprio objeto em questao
+#------------------------------------------
+#def é para definir funçoes
+#__init__ 'dois underlines' inicializa
+#self faz referencia ao proprio objeto em questao
 #def _init_(self, cor, marca, volume)
 '''
-class ControleRemoto:
+class ControleRemoto:    #AQUI ESTÁ FAZENDO AS DECLARAÇÕES
     def __init__(self, cor, marca, volume):
         self.cor=cor
         self.marca=marca
@@ -47,7 +57,7 @@ ferrari.estado='usado'
 
 print (fusca.estado)
 print (ferrari.estado)
-print(type(fusca))
+print(type(fusca)) diz se é string, int..
 print(type(ferrari))
 -----------------------------------  QUESTOES PARA PRATICAR
 #########1
@@ -109,6 +119,7 @@ print((type(ferrari)),ferrari.cor)
 
 
     
+
 
 
 
